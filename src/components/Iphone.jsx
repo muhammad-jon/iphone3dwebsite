f5a10cc634838f131a8cc7bca36207f3ac61722d
muhammad-jon <buyergaemail@gmail.com>
2024-10-01 14:39:26 +0500

    built

--- src/components/Iphone.jsx
+++ src/components/Iphone.jsx
@@ -10,7 +10,7 @@ import * as THREE from "three";
 import React, { useEffect, useRef } from "react";
 import { useGLTF, useTexture } from "@react-three/drei";
 
-function Model(props) {
+function Iphone(props) {
   const { nodes, materials } = useGLTF("/models/scene.glb");
 
   const texture = useTexture(props.item.img);
@@ -256,6 +256,6 @@ function Model(props) {
   );
 }
 
-export default Model;
+export default Iphone;
 
 useGLTF.preload("/models/scene.glb");
